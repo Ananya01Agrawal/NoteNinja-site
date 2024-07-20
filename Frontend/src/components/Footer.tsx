6b24a7b20ce9bef88dae64e4646a2ad17cf05edc
Ananya <ananyaagrawal073@gmail.com>
2024-07-20 19:48:04 +0530

    commit
    commit

--- Frontend/src/components/Footer.tsx
+++ Frontend/src/components/Footer.tsx
@@ -2,7 +2,6 @@ const Footer = () => {
 
   return (
     <footer className="py-2 text-lg text-center border-t text-light border-t-input">
-    Copyright  © {new Date().getFullYear()} | NotesNinja
     </footer>
   );
 };
